5729c58201b799ab767feb0e3c24c24768165b4c
Andyma <mady@akulaku.com>
2019-02-26 21:00:58 +0800

    快速排序

---
+++ app/src/main/java/andy319/io/exploresourcecode/testttt/QuickSort.java
@@ -0,0 +1,54 @@
+package andy319.io.exploresourcecode.testttt;
+
+/**
+ * 描述：快速排序
+ * 作者：mady@akulaku.com
+ * http://wiki.jikexueyuan.com/project/easy-learn-algorithm/fast-sort.html
+ * 时间： 2019/2/26
+ */
+public class QuickSort {
+
+    static int array[] = {8, 9, 3, 4, 5, 1, 2, 6, 7};
+
+    public static void main(String args[]) {
+        quickSort(0,array.length-1);
+        for (int i=0;i<array.length;i++){
+            System.out.println(i);
+        }
+
+
+    }
+    static void quickSort(int left,int right){
+        int i,j,t,temp;
+        if(left>right){
+            return;
+        }
+        temp=array[left];
+        i=left;
+        j=right;
+        while (i!=j){
+
+            while (array[j]>=temp&&i<j){ //先从右边找到比基准值小的
+                j--;
+            }
+            while (array[i]<=temp&&i<j){ //从左边找第一个比基准值大的
+                i++;
+            }
+            //将这两个值交换
+            if(i<j){
+                t=array[i];
+                array[i]=array[j];
+                array[j]=t;
+            }
+
+        }
+        //然后将基准值归位
+        array[left]=array[i];
+        array[i]=temp;
+        quickSort(left,i-1);
+        quickSort(i+1,right);
+
+    }
+
+
+}
